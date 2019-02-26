9bbd3a153d19f7f474047c1d0237356f626e8ef1
Magay Alex <magayalex@gmail.com>
2019-02-26 17:49:09 +0300

    setup eslint and travis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
-const mainFunc = name => {
-	console.log ('Welcome to the BRAIN GAMES!!!');
-	const userName = readlineSync.question('May i have your name? ');
-	console.log (`Hello, dear ${userName}`);
+
+const mainFunc = () => {
+  console.log('Welcome to the BRAIN GAMES!!!');
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, dear ${userName}`);
 };
 export default mainFunc;
