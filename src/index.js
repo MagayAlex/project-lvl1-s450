fc3503408f39f6b2836e334561ae5c849c9148dc
Magay Alex <magayalex@gmail.com>
2019-02-26 22:08:41 +0300

    fix problem, mentors suggest

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const mainFunc = () => {
+export default () => {
   console.log('Welcome to the BRAIN GAMES!!!');
   const userName = readlineSync.question('May i have your name? ');
   console.log(`Hello, dear ${userName}`);
 };
-export default mainFunc;
