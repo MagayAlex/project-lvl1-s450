9cbb1529a66c25ad0c5c414dad5d831296fe9623
Magay Alex <magayalex@gmail.com>
2019-03-02 00:19:19 +0300

    fix algoritm logic

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,29 +1,20 @@
 import {
   cons,
   l,
-  get,
-  head,
   toString,
   reverse,
 } from 'hexlet-pairs-data';
 import * as pairs from 'hexlet-pairs';
-import { randomNumber } from '../utils';
+import randomNumber from '../utils';
 import game from '..';
 
 const header = 'What number is missing in the progression?\n';
+const progressionStep = 3;
+const progressionLength = 10;
 
-const func = x => x + 3;
 const gameProgressionBody = () => {
   const startNumber = randomNumber(0, 100);
-  const progressionLength = 10;
-  const positionOfAnswer = randomNumber(0, progressionLength - 1);
-  const createList = (length, acc) => {
-    if (length === 1) {
-      return reverse(acc);
-    }
-    return createList(length - 1, cons(func(head(acc)), acc));
-  };
-  const list = createList(progressionLength, l(startNumber));
+  const positionOfAnswer = randomNumber(1, progressionLength);
   const listWithQuestion = (count, acc) => {
     if (count === progressionLength) {
       return reverse(acc);
@@ -31,10 +22,10 @@ const gameProgressionBody = () => {
     if (count === positionOfAnswer) {
       return listWithQuestion(count + 1, cons('..', acc));
     }
-    return listWithQuestion(count + 1, cons(get(count, list), acc));
+    return listWithQuestion(count + 1, cons(startNumber + count * progressionStep, acc));
   };
-  const question = toString(listWithQuestion(0, l()));
-  const answer = get(positionOfAnswer, list);
+  const question = toString(listWithQuestion(1, l(startNumber)));
+  const answer = startNumber + positionOfAnswer * progressionStep;
   return pairs.cons(question, String(answer));
 };
 
