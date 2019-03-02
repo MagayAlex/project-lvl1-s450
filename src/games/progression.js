9c42739635b4e3d9500023b2943aaa42fc2fa45e
Magay Alex <magayalex@gmail.com>
2019-03-02 15:17:14 +0300

    fix issue from step 7

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,32 +1,27 @@
-import {
-  cons,
-  l,
-  toString,
-  reverse,
-} from 'hexlet-pairs-data';
-import * as pairs from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 import randomNumber from '../utils';
 import game from '..';
 
 const header = 'What number is missing in the progression?\n';
-const progressionStep = 3;
 const progressionLength = 10;
 
 const gameProgressionBody = () => {
+  const progressionStep = randomNumber(0, 10);
   const startNumber = randomNumber(0, 100);
-  const positionOfAnswer = randomNumber(1, progressionLength);
+  const positionOfAnswer = randomNumber(0, progressionLength - 1);
   const listWithQuestion = (count, acc) => {
     if (count === progressionLength) {
-      return reverse(acc);
+      return acc;
     }
     if (count === positionOfAnswer) {
-      return listWithQuestion(count + 1, cons('..', acc));
+      return listWithQuestion(count + 1, `${acc} ..`);
     }
-    return listWithQuestion(count + 1, cons(startNumber + count * progressionStep, acc));
+    const newElement = startNumber + count * progressionStep;
+    return listWithQuestion(count + 1, `${acc} ${newElement}`);
   };
-  const question = toString(listWithQuestion(1, l(startNumber)));
+  const question = listWithQuestion(1, startNumber);
   const answer = startNumber + positionOfAnswer * progressionStep;
-  return pairs.cons(question, String(answer));
+  return cons(question, String(answer));
 };
 
 export default () => game(header, gameProgressionBody);
